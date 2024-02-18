4d9c4440d8cfba156972f5f466f2d99a6ac6e6d4
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-02-18 14:00:34 -0500

    probs sum to 1 test for pmf forecast function

--- tests/testthat/test-get_pmf_forecasts_from_quantile.R
+++ tests/testthat/test-get_pmf_forecasts_from_quantile.R
@@ -3,46 +3,63 @@ library(hubUtils)
 library(dplyr)
 library(tidyr)
 
+location_data <- data.frame(
+  stringsAsFactors = FALSE,
+  geo_value = "lc",
+  location = "111",
+  population = 1)
+  
+truth_data <- data.frame(
+  stringsAsFactors = FALSE,
+  geo_value = "lc",
+  time_value = as.Date("2021-12-18"),
+  target = "inc death", #assume the target is the same
+  value = 0)
+  
+quantile_inputs <- data.frame(
+  stringsAsFactors = FALSE,
+  model_id = "quantile-model",
+  location = "111",
+  reference_date = as.Date("2021-12-25"),
+  horizon = 1,
+  target = "inc death",
+  target_end_date = as.Date("2022-01-01"),
+  output_type = "quantile",
+  output_type_id = rep(NA, 21),
+  value = NA_real_)
+
+quantile_values <- runif(21, 0, 10) |> sort()
+output_prob <- seq(from=0, to=1, by=0.05)
+quantile_inputs$value <- quantile_values
+quantile_inputs$output_type_id <- output_prob
+
+horizons <- 1 
+categories <- c("large_increase", "increase", "stable", "decrease", "large_decrease")
+count_rate_multiplier <- matrix(c(9, 7, 3, 1), ncol=4)
+category_rule <- matrix(c(9, 7, 3, 1), ncol=4)
+criteria <- c(9, 7, 3, 1) # population * count_rate_multiplier
+
+test_that("category probabilities sum to 1", {
+  
+  pmf_output <- get_pmf_forecasts_from_quantile(quantile_inputs, location_data, truth_data, categories, horizons=1, count_rate_multiplier, category_rule, target_name="death rate change") 
+
+  pmf_output |>
+    dplyr::filter(output_type == "pmf") |>
+    dplyr::group_by(model_id, reference_date, horizon, target, target_end_date, location) |>
+    dplyr::summarize(prob_sum = sum(value)) |>
+    dplyr::distinct(prob_sum) |>
+    dplyr::pull(prob_sum) |>
+    expect_equal(1)
+})
 
 #can base this on quantile forecasts where the quantiles are from a known normal distribution, and truth/target data constructed so that the bin endpoints are easy/known quantities. this makes it so you can directly compute the probabilities using pnorm(upper, ...) - pnorm(lower, ...).
 
 test_that("category probabilities are correctly calculated", {
   # Quantile forecasts have quantiles from distribution F = N(5, 5)
-  location_data <- data.frame(
-    stringsAsFactors = FALSE,
-    geo_value = "lc",
-    location = "111",
-    population = 1)
-    
-  truth_data <- data.frame(
-    stringsAsFactors = FALSE,
-    geo_value = "lc",
-    time_value = as.Date("2021-12-18"),
-    target = "inc death", #assume the target is the same
-    value = 0)
-    
-  quantile_inputs <- data.frame(
-    stringsAsFactors = FALSE,
-    model_id = "quantile-model",
-    location = "111",
-    reference_date = as.Date("2021-12-25"),
-    horizon = 1,
-    target = "inc death",
-    target_end_date = as.Date("2022-01-01"),
-    output_type = "quantile",
-    output_type_id = rep(NA, 21),
-    value = NA_real_)
-
   quantile_values <- seq(from = 0, to = 10, by = 0.5) # expected
   output_prob <- stats::pnorm(quantile_values, mean = 5, 5)
   quantile_inputs$value <- quantile_values
   quantile_inputs$output_type_id <- output_prob
-
-  horizons <- 1 
-  categories <- c("large_increase", "increase", "stable", "decrease", "large_decrease")
-  count_rate_multiplier <- matrix(c(9, 7, 3, 1), ncol=4)
-  category_rule <- matrix(c(9, 7, 3, 1), ncol=4)
-  criteria <- c(9, 7, 3, 1) # population * count_rate_multiplier
 
   pmf_expected <- expand.grid(
     stringsAsFactors = FALSE,

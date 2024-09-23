758c02be1b96ff4b184076ca65d8e34638ee899d
Evan Ray <elray@umass.edu>
2024-09-23 12:14:40 -0400

    some minimal progress on setup for get_flusight_bin_endpoints

---
+++ R/get_flusight_bin_endpoints.R
@@ -0,0 +1,11 @@
+#' Get the bin endpoints for FluSight categorical targets
+#'
+#' @param target_ts Data frame with target time series data for FluSight,
+#' including the columns `location`, `date`, and `value`
+#' @param location_meta Data frame with metadata about locations for FluSight,
+#' matching the format of the file in cdcepi/FluSight-forecast-hub/auxiliary-data/locations.csv
+#'
+#' @return a data frame
+get_flusight_bin_endpoints <- function(target_ts, location_meta) {
+  
+}
